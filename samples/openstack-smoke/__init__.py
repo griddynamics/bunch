ec7659ea2a2bbe308e186228c2c077df39185794
Sergey Kosyrev <skosyrev@griddynamics.com>
2011-11-21 18:00:44 +0400

    Restructured sample Openstack bunch

--- samples/openstack-smoke/__init__.py
+++ samples/openstack-smoke/__init__.py
@@ -1,71 +1,28 @@
 from lettuce import step, world
+from nose.tools import assert_equals, assert_true
 import utils
 
-@step(u'Install "(.*)"')
-def install_group1(step, group1):
-    utils.rpm_install(group1)
 
-@step(u'Check if "(.*)" is not installed')
-def check_if_group1_is_not_installed(step, name):
-    utils.rpm_check(name, installed=False)
 
-@step(u'Check if "(.*)" is installed')
-def check_if_group1_is_installed(step, name):
-    utils.rpm_check(name)
+@step(u'current user can execute sudo without password')
+def check_current_user_sudo_nopwd(step):
+    assert_true(utils.check_can_execute_sudo_without_pwd())
 
-@step(u'Change flags in "([^"]*)":')
-def change_flags_in_group1(step, file_name):
-    values={}
+
+@step(u'every RPM package available:')
+def check_rpm_available(step):
     for data in step.hashes:
-        values[data['Flag']]=data['Value']
-    utils.fix_nova_conf(conf_file=file_name, flags_to_fix=values)
+        assert_true(utils.rpm_available(data['PackageName']))
 
 
-@step(u'Start services:')
-def start_services(step):
+@step(u'I install RPM package\(s\):')
+def install_rpm(step):
+    utils.rpm_clean_all_cached_data()
     for data in step.hashes:
-        utils.start_service(data['ServiceName'])
-
-@step(u'Create database "([^"]*)" with data "([^"]*)"')
-def create_database_group1_with_data_group2(step, name, script):
-    utils.mysql_create_database(database=name, sql_file=script)
+        assert_true(utils.rpm_install(data['PackageName']))
 
 
-@step(u'Check database "([^"]*)" exists')
-def check_database_group1(step, dbname):
-    utils.mysql_check_database(dbname)
-
-
-@step(u'Check database "([^"]*)" not exists')
-def check_database_group1(step, dbname):
-    utils.mysql_check_miss_database(dbname)
-
-@step(u'Sync database')
-def sync_database(step):
-    utils.sync_db()
-
-
-@step(u'Check services running:')
-def check_openstack_services_running(step):
+@step(u'every RPM package installed:')
+def check_rpm_installed(step):
     for data in step.hashes:
-        servicename = data['ServiceName']
-        utils.check_service(servicename)
-
-@step(u'Check services NOT running:')
-def check_openstack_services_not_running(step):
-    for data in step.hashes:
-        servicename = data['ServiceName']
-        utils.check_service(servicename, is_running=False)
-
-
-@step(u'Create admin user "(.*)"')
-def create_admin_user_group1(step, name):
-    utils.create_admin_user(name)
-
-@step(u'Check user "(.*)" exist')
-def check_user_group1_exist(step, group1):
-    utils.check_user(group1)
-
-@step(u'Check user "(.*)" does not exist')
-def check_user_group1_not_exist(step, group1):
-    utils.check_user(group1, False)
+        assert_true(utils.rpm_installed(data['PackageName']))
