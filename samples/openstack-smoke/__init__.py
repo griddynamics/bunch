b320479d40e3005a5580e97489447fed6c76158e
Sergey Kosyrev <skosyrev@griddynamics.com>
2011-11-29 12:09:46 +0400

    Added setup fixtures for OpenStack smoke test

--- samples/openstack-smoke/__init__.py
+++ samples/openstack-smoke/__init__.py
@@ -1,8 +1,20 @@
 from lettuce import step, world
-from nose.tools import assert_equals, assert_true
+from nose.tools import assert_equals, assert_true, assert_false
 import utils
+import os
 
+path = os.path.abspath(__file__)
+dir_path = os.path.dirname(path)
+utils.init(dir_path)
+config_file = os.path.join(dir_path, "config.yaml")
+config = utils.load_yaml_config(config_file)
 
+def dump(obj):
+  for attr in dir(obj):
+    print "obj.%s = %s" % (attr, getattr(obj, attr))
+
+mysql_admin = config['db']['admin']
+mysql_admin_pwd = config['db']['admin_pwd']
 
 @step(u'current user can execute sudo without password')
 def check_current_user_sudo_nopwd(step):
@@ -22,7 +34,78 @@ def install_rpm(step):
         assert_true(utils.rpm_install(data['PackageName']))
 
 
-@step(u'every RPM package installed:')
+@step(u'every RPM package is installed:')
 def check_rpm_installed(step):
     for data in step.hashes:
         assert_true(utils.rpm_installed(data['PackageName']))
+
+@step(u'I remove RPM package\(s\):')
+def remove_rpm(step):
+    utils.rpm_clean_all_cached_data()
+    for data in step.hashes:
+        assert_true(utils.rpm_remove(data['PackageName']))
+
+@step(u'every RPM package is not installed:')
+def check_rpm_not_installed(step):
+    for data in step.hashes:
+        assert_false(utils.rpm_installed(data['PackageName']))
+
+@step(u'I create MySQL database "(.*)"')
+def create_mysql_db(step, db_name):
+    assert_true(utils.mysql_create_db(db_name, mysql_admin, mysql_admin_pwd))
+
+@step(u'I grant all privileges on database "(.*)" to user "(.*)" identified by password "(.*)" at hosts:')
+def setup_mysql_access_for_hosts(step, db_name, db_user, db_pwd):
+    for data in step.hashes:
+        assert_true(utils.grant_db_access_for_hosts(data['Hostname'],db_name, db_user, db_pwd, mysql_admin, mysql_admin_pwd))
+
+@step(u'I grant all privileges on database "(.*)" to user "(.*)" identified by password "(.*)" locally')
+def setup_mysql_access_local(step, db_name, db_user, db_pwd):
+    assert_true(utils.grant_db_access_local(db_name, db_user, db_pwd, mysql_admin, mysql_admin_pwd))
+    assert_true(utils.grant_db_access_local(db_name, mysql_admin, mysql_admin_pwd, mysql_admin, mysql_admin_pwd))
+
+@step(u'every service is running:')
+def every_service_is_running(step):
+    for data in step.hashes:
+        assert_true(utils.check_service_is_running(data['ServiceName']))
+
+@step(u'I start services:')
+def start_services(step):
+    for data in step.hashes:
+        assert_true(utils.start_service(data['ServiceName']))
+
+
+@step(u'MySQL database "(.*)" exists')
+def mysql_db_exists(step, db_name):
+    print db_name
+    assert_true(utils.check_mysql_db_exists(db_name, mysql_admin, mysql_admin_pwd))
+
+
+@step(u'user "(.*)" has all privileges on database "(.*)"')
+def mysql_user_has_all_privileges(step, user, db_name):
+    assert_true(utils.check_mysql_user_has_all_privileges_on_db(user, db_name, mysql_admin, mysql_admin_pwd))
+
+@step(u'I perform Nova DB sync')
+def perform_nova_db_sync(step):
+    assert_true(utils.perform_nova_db_sync())
+
+
+@step(u'I stop services:')
+def stop_services(step):
+    for data in step.hashes:
+        assert_true(utils.stop_service(data['ServiceName']))
+
+@step(u'every service is stopped:')
+def every_service_is_stopped(step):
+    for data in step.hashes:
+        assert_true(utils.check_service_is_stopped(data['ServiceName']))
+
+@step(u'I clean state files:')
+def clean_state_files(step):
+    for data in step.hashes:
+        assert_true(utils.remove_files_recursively_forced(data['PathWildCard']))
+
+@step(u'no files exist:')
+def no_files_exist(step):
+    for data in step.hashes:
+        assert_true(utils.check_no_files_exists(data['PathWildCard']))
--- samples/openstack-smoke/utils.py
+++ samples/openstack-smoke/utils.py
@@ -2,12 +2,22 @@ import commands
 import os
 import time
 import re
+import yaml
+import glob
 
 
 
-log_dir =  os.path.dirname(os.path.abspath(__file__))
 
-bash_log = os.path.join(log_dir, "bash_out.log")
+def init(dir_path):
+    global env_dir_path, bash_log
+    env_dir_path = dir_path
+    bash_log = os.path.join(env_dir_path, "bash.log")
+
+
+def load_yaml_config(filename):
+    with open(filename, 'r') as config_file:
+        config = yaml.load(config_file)
+        return config
 
 
 def log(logfile, message):
@@ -26,7 +36,6 @@ def cmd_output_contains_pattern(retcode, pattern):
     return (not search_result is None) and len(search_result.group()) > 0
 
 
-
 def bash(cmd):
     log(bash_log, "[CMD] " + cmd)
     retcode = commands.getstatusoutput(cmd)
@@ -36,13 +45,25 @@ def bash(cmd):
 def rpm_clean_all_cached_data():
     bash("sudo yum -q clean all")
 
+def remove_files_recursively_forced(wildcard):
+    out = bash("sudo rm -rf %s" % wildcard)
+    return cmd_successful(out)
+
+def check_no_files_exists(wildcard):
+    out = bash("sudo ls -1 %s | wc -l" % wildcard)
+    return cmd_successful(out) and cmd_output_contains_pattern(out, "(\s)*0(\s)*")
+
+    
+
+
 def rpm_install(package):
-    out = bash("sudo yum -y install %s" % package)
-    return cmd_output_contains_pattern(out, "Installed:[\s\S]*%s.*" % package)
+    out = bash("sudo yum -y install '%s'" % package)
+    return cmd_successful(out) and cmd_output_contains_pattern(out, "(Installed:[\s\S]*%s.*)|(Package.*%s.* already installed)" % (package, package))
 
 
 def rpm_remove(package):
-    bash("sudo yum -q -y erase %s" % package)
+    out = bash("sudo yum -y erase '%s'" % package)
+    return cmd_output_contains_pattern(out, "(No Match for argument)|(Removed:[\s\S]*%s.*)|(Package.*%s.*not installed)" % (package, package))
 
 
 def rpm_installed(package):
@@ -58,5 +79,66 @@ def rpm_available(package):
 
 def check_can_execute_sudo_without_pwd():
     out = bash("sudo -l")    
-    return cmd_successful(out) and cmd_output_nonempty(out) and cmd_output_contains_pattern(out, "\(ALL\)(\s)*NOPASSWD:(\s)*ALL")
+    return cmd_successful(out) and cmd_output_nonempty(out) \
+        and cmd_output_contains_pattern(out, "\(ALL\)(\s)*NOPASSWD:(\s)*ALL")
 
+
+
+def mysql_create_db(db_name, admin_name="root", admin_pwd="root"):
+    bash("mysqladmin -u%s -p%s -f drop %s" % (admin_name, admin_pwd, db_name))
+    out = bash("mysqladmin -u%s -p%s create %s" % (admin_name, admin_pwd, db_name))
+    return cmd_successful(out)
+
+def exec_mysql_command(sql_command, admin_name="root", admin_pwd="root"):
+    out = bash('echo "%s" | mysql -u%s -p%s mysql' % (sql_command, admin_name, admin_pwd))
+    return out
+
+def grant_db_access_for_hosts(hostname,db_name, db_user, db_pwd, admin_name="root", admin_pwd="root"):
+    sql_command =  "GRANT ALL PRIVILEGES ON %s.* TO '%s'@'%s' IDENTIFIED BY '%s';" % (db_name, db_user, hostname, db_pwd)
+    return cmd_successful(exec_mysql_command(sql_command, admin_name, admin_pwd))
+
+def grant_db_access_local(db_name, db_user, db_pwd, admin_name="root", admin_pwd="root"):
+    sql_command =  "GRANT ALL PRIVILEGES ON %s.* TO %s IDENTIFIED BY '%s';" % (db_name, db_user, db_pwd)
+    return cmd_successful(exec_mysql_command(sql_command, admin_name, admin_pwd))
+
+def check_mysql_db_exists(db_name, admin_name="root", admin_pwd="root"):
+    sql_command = "show databases;"
+    out = exec_mysql_command(sql_command, admin_name, admin_pwd)
+    return cmd_successful(out) and cmd_output_contains_pattern(out, "%s" % db_name)
+
+def check_mysql_user_has_all_privileges_on_db(username, db_name, admin_name="root", admin_pwd="root"):
+    sql_command = "show grants for '%s'@'localhost';" %username
+    out = exec_mysql_command(sql_command, admin_name, admin_pwd)
+    return cmd_successful(out) \
+        and cmd_output_contains_pattern(out, "GRANT ALL PRIVILEGES ON .*%s.* TO .*%s.*" % (db_name, username))
+
+def start_service(service):
+    out = bash("sudo service %s start" % service)
+    return cmd_successful(out)
+
+
+def stop_service(service):
+    out = bash("sudo service %s stop" % service)
+    return cmd_successful(out)
+
+
+def check_service_is_running(service):
+    out = bash("sudo service %s status" % service)
+    return cmd_successful(out) \
+        and cmd_output_contains_pattern(out, "(?i)running") \
+        and (not cmd_output_contains_pattern(out, "(?i)stopped|unrecognized|dead"))
+
+def check_service_is_stopped(service):
+    out = bash("sudo service %s status" % service)
+    unusual_service_patterns = {'rabbitmq-server': 'no_nodes_running'}
+
+    if service in unusual_service_patterns:
+        return cmd_output_contains_pattern(out, unusual_service_patterns[service])
+
+    return (not cmd_output_contains_pattern(out, "(?i)running")) \
+        and cmd_output_contains_pattern(out, "(?i)stopped|unrecognized|dead")
+
+
+def perform_nova_db_sync():
+    out = bash("sudo nova-manage db sync")
+    return cmd_successful(out)
